be05647fbf500a970a5a6277e76807eb7ebd272d
Maxime Winter-knecht <mwinter-@k0r2p5.42mulhouse.fr>
2022-04-11 15:01:42 +0200

    lol

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,26 +6,26 @@
 /*   By: mwinter- <mwinter-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 16:10:10 by mwinter-          #+#    #+#             */
-/*   Updated: 2022/04/10 16:36:42 by mwinter-         ###   ########.fr       */
+/*   Updated: 2022/04/11 14:37:51 by mwinter-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header/so_long.h"
-
-int	theosucemonbeat(int keycode, t_vars *vars)
-{
-	ft_printf("keycode = %d\n",keycode);
-//	if (keycode == 53)
-//		mlx_destroy_window(vars->mlx, vars->win);
-	return (0);
-}
+#include <mlx.h>
 
 int	main(void)
 {
-	t_vars	vars;
 
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
-	mlx_hook(vars.win, 2, 1L<<0, theosucemonbeat, &vars);
-	mlx_loop(vars.mlx);
+	void	*mlx;
+	void	*img;
+	char	*relative_path = "./test.xpm";
+	int		img_width;
+	int		img_height;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
+	mlx_put_image_to_window(mlx, mlx_win, img, 0, 0);
+	mlx_loop(mlx);
 }
