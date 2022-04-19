46bebe6ff74ca3ef8b155cc1d5429f55c9facdc0
Maxime Winter-knecht <mwinter-@k0r2p8.42mulhouse.fr>
2022-04-19 18:08:28 +0200

    lol

--- so_long/header/so_long.h
+++ so_long/header/so_long.h
@@ -6,27 +6,30 @@
 /*   By: mwinter- <mwinter-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 13:04:21 by mwinter-          #+#    #+#             */
-/*   Updated: 2022/04/14 15:18:00 by mwinter-         ###   ########.fr       */
+/*   Updated: 2022/04/19 17:10:21 by mwinter-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 
-#define SO_LONG_H
+# define SO_LONG_H
 
-#include <mlx.h>
-#include "../header/libft.h"
-#include <fcntl.h>
-#include <unistd.h>
+# include <mlx.h>
+# include "../header/libft.h"
+# include <fcntl.h>
+# include <unistd.h>
 
-typedef struct s_data {
-
+typedef struct s_data
+{
 	void	*mlx;
 	void	*img;
 	char	*relative_path;
 	int		img_width;
 	int		img_height;
 	void	*mlx_win;
+	int		p_x;
+	int		p_y;
+	char	**tab;
 
 }	t_data;
 
--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,148 +6,198 @@
 /*   By: mwinter- <mwinter-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 16:10:10 by mwinter-          #+#    #+#             */
-/*   Updated: 2022/04/14 16:12:29 by mwinter-         ###   ########.fr       */
+/*   Updated: 2022/04/19 17:42:40 by mwinter-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header/so_long.h"
 
-
-void	print_error(char * msg)
+void	print_error(char *msg)
 {
-	ft_printf("Error : %s",msg);
+	ft_printf("Error : %s", msg);
 	exit(0);
 }
 
-void	put_sprite(t_data *lol, char letter, int counter , int second_counter)
+void	put_sprite(t_data *lol, char letter, int i, int second_i)
 {
 	if (letter == 'E')
 		lol->relative_path = "sprite/exit.xpm";
 	else if (letter == 'W')
-		lol ->relative_path = "sprite/wall.xpm";
+		lol->relative_path = "sprite/wall.xpm";
 	else if (letter == 'F')
 		lol->relative_path = "sprite/floor3.xpm";
 	else if (letter == 'P')
-		lol ->relative_path = "sprite/player_r.xpm";
+	{
+		lol->relative_path = "sprite/player_r.xpm";
+		lol->p_x = second_i;
+		lol->p_y = i;
+		lol->tab[i][second_i] = 'F';
+	}
 	else if (letter == 'C')
-		lol ->relative_path = "sprite/collect.xpm";
+	{
+		lol->relative_path = "sprite/collect.xpm";
+	}
 	else
 		print_error("Caractere indefinis");
-	lol->img = mlx_xpm_file_to_image(lol->mlx, lol->relative_path, &lol->img_width, &lol->img_height);
-	mlx_put_image_to_window(lol->mlx, lol->mlx_win, lol->img, (second_counter * 32), (counter * 32));
+	lol->img = mlx_xpm_file_to_image(lol->mlx, lol->relative_path,
+			&lol->img_width, &lol->img_height);
+	mlx_put_image_to_window(lol->mlx, lol->mlx_win, lol->img, (second_i * 32),
+		(i * 32));
 	mlx_destroy_image(lol->mlx, lol->img);
 }
 
-void	map_reader(char **tab, t_data *lol)
+void	map_reader(t_data *lol)
 {
-	int	counter;
-	int	second_counter;
+	int	i;
+	int	second_i;
 
-	counter = 0;
-	while(tab[counter])
+	i = 0;
+	while (lol->tab[i])
 	{
-		second_counter = 0;
-		while (tab[counter][second_counter])
+		second_i = 0;
+		while (lol->tab[i][second_i])
 		{
-			put_sprite(lol,tab[counter][second_counter],counter,second_counter);
-		second_counter ++;
+			put_sprite(lol, lol->tab[i][second_i], i, second_i);
+			second_i++;
 		}
-		counter ++;
+		i++;
 	}
 }
 
-int		moove(int keycode,t_data *lol)
+int	moove_allower(t_data *lol, int keycode)
 {
-	static int pos_x;							//satic!!!!!!!!!!!!!!!!!!!!!!
-	static int pos_y;							//satic!!!!!!!!!!!!!!!!!!!!!!
-
-	// if (pos_x == 0)
-	// 	pos_x = 20;
-	// if (pos_y == 0)
-	// 	pos_y = 20;
-	//mlx_put_image_to_window(lol->mlx, lol->mlx_win, lol->img, 500, 500);
-	//ft_printf("mlx = %d\n",lol->mlx);
-	ft_printf("%d",keycode);
-
-	if (keycode == 2)
-	{
-		mlx_put_image_to_window(lol->mlx, lol->mlx_win, lol->img, pos_x, pos_y);
-		pos_x += 10;
-	}
-	else if (keycode == 1)
-	{
-		mlx_put_image_to_window(lol->mlx, lol->mlx_win, lol->img, pos_x, pos_y);
-		pos_y += 10;
-	}
+	if (keycode == 2 && (lol->tab[lol->p_y][lol->p_x + 1] == 'F'
+		|| lol->tab[lol->p_y][lol->p_x + 1] == 'C'))
+		return (1);
+	else if (keycode == 13 && (lol->tab[lol->p_y - 1][lol->p_x] == 'F'
+		|| lol->tab[lol->p_y - 1][lol->p_x] == 'C'))
+		return (1);
+	else if (keycode == 0 && (lol->tab[lol->p_y][lol->p_x - 1] == 'F'
+		|| lol->tab[lol->p_y][lol->p_x - 1] == 'C'))
+		return (1);
+	else if (keycode == 1 && (lol->tab[lol->p_y + 1][lol->p_x] == 'F'
+		|| lol->tab[lol->p_y + 1][lol->p_x] == 'C'))
+		return (1);
+	else if (keycode == 2 && (lol->tab[lol->p_y][lol->p_x + 1] == 'E'))
+		return (2);
+	else if (keycode == 13 && (lol->tab[lol->p_y - 1][lol->p_x] == 'E'))
+		return (2);
+	else if (keycode == 0 && (lol->tab[lol->p_y][lol->p_x - 1] == 'E'))
+		return (2);
+	else if (keycode == 1 && (lol->tab[lol->p_y + 1][lol->p_x] == 'E'))
+		return (2);
 	return (0);
 }
 
-char *one_line_parsing(int fd, int *nb_line)
+int	end_checker(char **tab)
 {
-	char *buff;
-	char *old;
-	char *oneDTab;
+	int	i;
+	int	second_i;
+
+	i = 0;
+	while (tab[i])
+	{
+		second_i = 0;
+		while (tab[i][second_i])
+		{
+			if (tab[i][second_i] == 'C')
+				return (0);
+			second_i++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+void	succes(t_data *lol)
+{
+	int	i;
+
+	mlx_destroy_window(lol->mlx, lol->mlx_win);
+	i = 0;
+	while (lol->tab[i])
+	{
+		free(lol->tab[i]);
+		i++;
+	}
+	free(lol->tab);
+	ft_printf("GG trop fort le mec enft");
+	exit(0);
+}
+
+int	moove(int keycode, t_data *lol)
+{
+	if (keycode == 2 && moove_allower(lol, keycode) == 1)
+	{
+		put_sprite(lol, 'F', lol->p_y, lol->p_x);
+		put_sprite(lol, 'P', lol->p_y, ++lol->p_x);
+	}
+	else if (keycode == 13 && moove_allower(lol, keycode) == 1)
+	{
+		put_sprite(lol, 'F', lol->p_y, lol->p_x);
+		put_sprite(lol, 'P', --lol->p_y, lol->p_x);
+	}
+	else if (keycode == 0 && moove_allower(lol, keycode) == 1)
+	{
+		put_sprite(lol, 'F', lol->p_y, lol->p_x);
+		put_sprite(lol, 'P', lol->p_y, --lol->p_x);
+	}
+	else if (keycode == 1 && moove_allower(lol, keycode) == 1)
+	{
+		put_sprite(lol, 'F', lol->p_y, lol->p_x);
+		put_sprite(lol, 'P', ++lol->p_y, lol->p_x);
+	}
+	else if (moove_allower(lol, keycode) == 2 && end_checker(lol->tab))
+		succes(lol);
+	return (0);
+}
+
+char	*one_line_parsing(int fd, int *nb_line)
+{
+	char	*buff;
+	char	*old;
+	char	*one_d_tab;
 
 	buff = NULL;
-	old= NULL;  //3x inutile?
-	oneDTab=NULL;
-	
+	old = NULL;
+	one_d_tab = NULL;
 	buff = get_next_line(fd);
-
 	while (buff)
 	{
 		old = strdup(buff);
 		buff = get_next_line(fd);
-		oneDTab = ft_strjoin(oneDTab, old);
-		*nb_line  = *nb_line + 1;
+		one_d_tab = ft_strjoin(one_d_tab, old);
+		*nb_line = *nb_line + 1;
 	}
-	if (!oneDTab)
+	if (!one_d_tab)
 		print_error("Pas de map dans le fichier");
-
-	return(oneDTab);
-
+	return (one_d_tab);
 }
 
 int	main(void)
 {
 	t_data	lol;
-	int		counter;
+	int		i;
 	int		line_size;
-	char	**tab;
-	int nb_line = 0;
-	int fd;
+	int		nb_line;
+	int		fd;
 
-	counter = 0;
-
-	
-	fd = open("map",O_RDONLY);
+	nb_line = 0;
+	i = 0;
+	fd = open("map", O_RDONLY);
 	if (fd == -1)
 		print_error("Pas de map a ce nom");
-
-	tab = ft_split(one_line_parsing(fd, &nb_line), '\n'); 
-	
-	line_size = ft_strlen(tab[0]);
-
-////////////////////////////////////////////////////////error verif
-	while (tab[counter])
-	{
-		//ft_printf("line_size = %d",ft_strlen(tab[counter]));
-		if (ft_strlen(tab[counter++]) != ((size_t)line_size))
+	lol.tab = ft_split(one_line_parsing(fd, &nb_line), '\n');
+	line_size = ft_strlen(lol.tab[0]);
+	while (lol.tab[i])
+		if (ft_strlen(lol.tab[i++]) != ((size_t)line_size))
 			print_error("lignes pas a la meme taille");
-	}
-////////////////////////////////////////////////////
-;
 	lol.mlx = mlx_init();
-
-	//ft_printf("line_size = %d, nb_line = %d ",line_size,nb_line);
-	lol.mlx_win = mlx_new_window(lol.mlx, (line_size * 32 +1), (nb_line * 32+1), "Pacman_42");
-
-
-	counter = 0;
-	
-map_reader(tab, &lol);
-/////////////////////////////////
-
-	//mlx_hook(lol.mlx_win, 2, 1L << 0, moove, &lol);
+	lol.mlx_win = mlx_new_window(lol.mlx, (line_size * 32 + 1), (nb_line * 32
+				+ 1), "Pacman_42");
+	i = 0;
+	map_reader(&lol);
+	mlx_hook(lol.mlx_win, 2, 1L << 0, moove, &lol);
 	mlx_loop(lol.mlx);
+	return (0);
 }
