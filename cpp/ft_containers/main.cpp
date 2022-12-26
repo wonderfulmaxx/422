e946c0c4c96daca72f83dc924a0ea891b93e2056
wonderfulmaxx <gingembresole@my.com>
2022-12-26 11:28:42 -0500

    lol

--- cpp/ft_containers/main.cpp
+++ cpp/ft_containers/main.cpp
@@ -5,10 +5,37 @@ int main()
 
 	ft::vector<int> vector_int;
 
-	ft::vector<int> vector_long(4,2);
+	//ft::vector<int> vector_long(4,2);
 
-	std::cout << vector_long.at(2);
+	vector_int.push_back(8);
+	vector_int.push_back(666); 
+
+
+	std::cout << vector_int[0] << std::endl;
+	std::cout << vector_int[1] << std::endl;
+
+	vector_int.pop_back();
+
+	std::cout << vector_int[1] << std::endl;
+
+	vector_int.push_back(777);
+
+	std::cout << vector_int[1] << std::endl;
+
+	//std::cout << "capacity = " << vector_long.capacity() << std::endl;
+
+	//std::cout << "test here" ;
+	 //vector_long.reserve(15);
+
+	// std::cout << "capacity = " << vector_long.capacity() << std::endl;
+
+	//std::cout << vector_int.front() << std::endl;
+
+	//std::cout << "Max size =" << vector_int.max_size() << std::endl;
 
 //	ft::vector<int>::iterator lol;
+
+	std::cout << std::endl << "attention pas de flags dans le makefile \n\
+	+ verif pop back pop au bon endroit?" << std::endl;
 
 }
--- cpp/ft_containers/vector.hpp
+++ cpp/ft_containers/vector.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <memory>
 #include "vector_iterator.hpp"
+//#include <cstddef>
 
 namespace ft
 {
@@ -17,6 +18,7 @@ namespace ft
 			typedef T value_type;
 			typedef size_t size_type;
 			typedef typename allocator_type::reference reference;
+			typedef typename allocator_type::reference const_reference;
 
 			explicit vector (const allocator_type& alloc = allocator_type())
 			:
@@ -44,7 +46,23 @@ namespace ft
 				}
 			}
 
+			// vector & operator = (const vector &rhs)
+			// 	:
+			// 	_alloc(alloc),
+			// 	_start(rhs._start),
+			// 	_end(nullptr),
+			// 	_end_capacity(nullptr)
+			// {
+				
+			// }
+
+
 			reference operator[](size_type n)
+			{
+				return(*(_start + n));
+			}
+
+			const_reference operator[] (size_type n) const
 			{
 				return(*(_start + n));
 			}
@@ -55,25 +73,117 @@ namespace ft
 				return((*this)[pos]);
 			}
 
-			// const_reference at( size_type pos ) const
+			const_reference at( size_type pos ) const
+			{
+				range_check(pos);
+				return((*this)[pos]);
+			}
+
+			reference front()
+			{
+				return((*this)[0]);
+			}
+
+			const_reference front() const
+			{
+				return((*this)[0]);
+			}
+
+			// void push_back (const value_type& val)
 			// {
-			// 	return()
+			// 	_alloc.allocate(1,_end);
+			// 	_end++;
+			// 	_end_capacity ++;
+			// 	_alloc.construct(_end,val);
 			// }
+
+			size_type capacity() const
+			{
+				return(this->_end_capacity - this->_start);
+			}
+
+			void reserve( size_type new_cap )
+			{
+				if (new_cap > this->capacity())
+					if (new_cap <= this->max_size())
+						{
+							pointer prev_start = this->_start;
+							pointer prev_start_const = this->_start;
+							pointer prev_end = this->_end;
+							
+
+							size_type prev_size = this->size();
+							size_type prev_capacity = this->capacity();
+
+							this->_start =_alloc.allocate(new_cap);
+							this->_end = this->_start;
+							this->_end_capacity = new_cap + this->_start;
+
+							
+
+							while (prev_start != prev_end)
+							{
+								_alloc.construct(this->_end,*prev_start);
+								prev_start ++;
+								this->_end ++;
+							}
+							_alloc.deallocate(prev_start_const, prev_capacity);
+						}
+					else
+					{
+						throw(std::length_error("New reserved capacity is too big"));
+					}
+			}
+
+			size_type size() const
+			{
+				return(std::distance(this->_start, this->_end));
+			}
+
+			size_type max_size() const
+			{
+				return(allocator_type().max_size());
+			}
+
+			void push_back( const T& value )
+			{
+				if (this->_end == _end_capacity)
+				{
+					if (this ->size() == 0)
+						reserve (1);
+					else
+						reserve (this->capacity() * 2);
+				}
+				_alloc.construct(this->_end,value);
+				this->_end++;
+			}
+
+			void pop_back()
+			{
+				_alloc.destroy(this->_end - 1);
+				_end--;
+			}
+
+			void resize (size_type n, value_type val = value_type())
+			{
+				int counter = 0;
+				while (n + counter > this->size())
+				{
+					
+				}
+
+			}
 
 			typedef typename ft::vectorIterator<T> iterator; 
 
 			private:
 
-			void range_check(size_type n)
+			void range_check(size_type const n) const
 			{
 				if (n > this->size())
 					throw (std::out_of_range("you searched for a too high index that is not in the vector"));
 			}
 
-			size_type size()
-			{
-				return(std::distance(this->_start, this->_end));
-			}
 
 			allocator_type _alloc;
 			pointer _start;
